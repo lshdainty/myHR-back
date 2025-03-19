daeb264fb09451d13a79afa4ee486796fe266ea3
lshdainty <lshdainty@gmail.com>
2025-03-19 23:46:30 +0900

    user service testcase 추가 - 유저 리스트 조회 테스트 케이스 추가 - 유저 수정 테스트케이스 추가 - 유저 삭제 테스트케이스 추가

--- src/test/java/com/lshdainty/myhr/service/UserServiceTest.java
+++ src/test/java/com/lshdainty/myhr/service/UserServiceTest.java
@@ -10,6 +10,8 @@ import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
+import java.util.*;
+
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.mockito.ArgumentMatchers.any;
@@ -70,11 +72,101 @@ public class UserServiceTest {
 
     @Test
     @DisplayName("단건 유저 조회 테스트 (실패)")
-    void findUserFailsTest() {
+    void findUserFailTest() {
         // given
         given(userRepository.findById(999L)).willReturn(null);
 
         // when, then
         assertThrows(IllegalArgumentException.class, () -> userService.findUser(999L));
+    }
+
+    @Test
+    @DisplayName("전체 유저 조회 테스트")
+    void findUsersTest() {
+        // given
+        given(userRepository.findUsers())
+                .willReturn(List.of(
+                        User.createUser("이서준", "19700723", "ADMIN", "9 ~ 6", "N"),
+                        User.createUser("김서연", "19701026", "BP", "8 ~ 5", "N"),
+                        User.createUser("김지후", "19740115", "BP", "10 ~ 7", "Y")
+                )
+        );
+
+        // when
+        List<User> findUsers = userService.findUsers();
+
+        // then
+        assertThat(findUsers.size()).isEqualTo(3);
+        assertThat(findUsers)
+                .extracting(User::getName)
+                .containsExactlyInAnyOrder("이서준", "김서연", "김지후");
+    }
+
+    @Test
+    @DisplayName("유저 수정 테스트 (성공)")
+    void editUserSuccessTest() {
+        // given
+        String name = "이서준";
+        String birth = "19700723";
+        String workTime = "9 ~ 6";
+        String employ = "ADMIN";
+        String lunar = "N";
+        User user = User.createUser(name, birth, employ, workTime, lunar);
+
+        given(userRepository.findById(1L)).willReturn(user);
+
+        // when
+        name = "이하은";
+        workTime = "10 ~ 7";
+        userService.editUser(1L, name, null, null, workTime, null);
+
+        // then
+        assertThat(user.getName()).isEqualTo(name);
+        assertThat(user.getBirth()).isEqualTo(birth);
+        assertThat(user.getWorkTime()).isEqualTo(workTime);
+        assertThat(user.getEmploy()).isEqualTo(employ);
+        assertThat(user.getLunarYN()).isEqualTo(lunar);
+    }
+
+    @Test
+    @DisplayName("유저 수정 테스트 (실패)")
+    void editUserFailTest() {
+        // given
+        given(userRepository.findById(999L)).willReturn(null);
+
+        // when & then
+        assertThrows(IllegalArgumentException.class,
+                () -> userService.editUser(999L, "이하은", null, null, null, null));
+    }
+
+    @Test
+    @DisplayName("유저 삭제 테스트 (성공)")
+    void deleteUserSuccessTest() {
+        // given
+        String name = "이서준";
+        String birth = "19700723";
+        String workTime = "9 ~ 6";
+        String employ = "ADMIN";
+        String lunar = "N";
+        User user = User.createUser(name, birth, employ, workTime, lunar);
+
+        given(userRepository.findById(1L)).willReturn(user);
+
+        // when
+        userService.deleteUser(1L);
+
+        // then
+        assertThat(user.getDelYN()).isEqualTo("Y");
+    }
+
+    @Test
+    @DisplayName("유저 삭제 테스트 (실패)")
+    void deleteUserFailTest() {
+        // given
+        given(userRepository.findById(999L)).willReturn(null);
+
+        // when & then
+        assertThrows(IllegalArgumentException.class,
+                () -> userService.deleteUser(999L));
     }
 }
